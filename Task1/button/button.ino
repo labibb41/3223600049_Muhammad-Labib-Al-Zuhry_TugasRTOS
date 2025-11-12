d853f9b640b3fba6fbd5a73652aa880763e4a356
labibb41 <muhammadlabibalzuhry@gmail.com>
2025-11-12 17:02:56 +0700

    Button

--- Task1/button/button.ino
+++ Task1/button/button.ino
@@ -1,31 +1,65 @@
-#define BUTTON1 4
-#define BUTTON2 7
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
 
-void TaskButton(void *pvParameters) {
-  pinMode(BUTTON1, INPUT_PULLUP);
-  pinMode(BUTTON2, INPUT_PULLUP);
+// Definisi pin tombol
+#define BUTTON1_PIN 4
+#define BUTTON2_PIN 5
 
-  while (1) {
-    if (digitalRead(BUTTON1) == LOW) {
-      Serial.println("Button 1 pressed!");
-      vTaskDelay(200 / portTICK_PERIOD_MS);  // debounce
+// Variabel global untuk menyimpan status
+volatile int button1_state = 1;
+volatile int button2_state = 1;
+
+// ---------------------------------------------------------------------
+// Task 1 - Membaca tombol 1 di Core 0
+// ---------------------------------------------------------------------
+void task_button1(void *pvParameter) {
+    gpio_set_direction(BUTTON1_PIN, GPIO_MODE_INPUT);
+    gpio_pullup_en(BUTTON1_PIN);
+
+    while (1) {
+        int current = gpio_get_level(BUTTON1_PIN);
+        if (current != button1_state) {
+            button1_state = current;
+            if (current == 0) {
+                printf("[Core %d] Button 1 Pressed\n", xPortGetCoreID());
+            } else {
+                printf("[Core %d] Button 1 Released\n", xPortGetCoreID());
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(50)); // debounce
     }
-
-    if (digitalRead(BUTTON2) == LOW) {
-      Serial.println("Button 2 pressed!");
-      vTaskDelay(200 / portTICK_PERIOD_MS);  // debounce
-    }
-
-    vTaskDelay(10 / portTICK_PERIOD_MS);
-  }
 }
 
-void setup() {
-  Serial.begin(115200);
+// ---------------------------------------------------------------------
+// Task 2 - Membaca tombol 2 di Core 1
+// ---------------------------------------------------------------------
+void task_button2(void *pvParameter) {
+    gpio_set_direction(BUTTON2_PIN, GPIO_MODE_INPUT);
+    gpio_pullup_en(BUTTON2_PIN);
 
-  // Jalankan di Core 0 / Core 1 (pilih salah satu)
-  //xTaskCreatePinnedToCore(TaskButton, "BUTTONS", 2048, NULL, 1, NULL, 0); // core 0
-  xTaskCreatePinnedToCore(TaskButton, "BUTTONS", 2048, NULL, 1, NULL, 1); // core 1
+    while (1) {
+        int current = gpio_get_level(BUTTON2_PIN);
+        if (current != button2_state) {
+            button2_state = current;
+            if (current == 0) {
+                printf("[Core %d] Button 2 Pressed\n", xPortGetCoreID());
+            } else {
+                printf("[Core %d] Button 2 Released\n", xPortGetCoreID());
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(50));
+    }
 }
 
-void loop() {}
+// ---------------------------------------------------------------------
+// Fungsi utama RTOS
+// ---------------------------------------------------------------------
+void app_main() {
+    printf("=== FreeRTOS Dual Core Button Test (ESP32-S3) ===\n");
+
+    // Membuat task dan menetapkannya ke core tertentu
+    xTaskCreatePinnedToCore(task_button1, "Button1 Task", 2048, NULL, 1, NULL, 0); // Core 0
+    xTaskCreatePinnedToCore(task_button2, "Button2 Task", 2048, NULL, 1, NULL, 1); // Core 1
+}
